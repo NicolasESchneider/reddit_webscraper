17d9741aa6590d302d57bd65619bd8a8a8c2e2cd
Nicolas Schneider <nicolas.e.schneider@vanderbilt.edu>
2020-09-18 14:29:30 -0400

    change log message

--- tts.js
+++ tts.js
@@ -4,6 +4,6 @@ export const saveAsMP3 = (text) => {
     const gtts = new gTTS(text, 'en');
     gtts.save('Voice.mp3', function (err, result){ 
         if(err) { throw new Error(err); } 
-        console.log("Text to speech converted!"); 
+        console.log("~~Successful MP3 Conversion~~"); 
     });
 }
