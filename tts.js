acf95898131814f06398fdb8daf42df46db4f220
Nicolas Schneider <nicolas.e.schneider@vanderbilt.edu>
2020-09-22 23:47:07 -0400

    formatting

--- tts.js
+++ tts.js
@@ -3,7 +3,9 @@ import gTTS from 'gtts';
 export const saveAsMP3 = (text) => {
     const gtts = new gTTS(text, 'en');
     gtts.save('VoiceOver.mp3', function (err, result){ 
-        if(err) { throw new Error(err); } 
+        if (err) {
+            throw new Error(err);
+        } 
         console.log("~~Successful MP3 Conversion~~"); 
     });
 }
